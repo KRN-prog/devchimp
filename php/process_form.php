693737280c5f449527540d3e752aaf5b7ce18513
Kyrian <kyrian.arvene@gmail.com>
2025-05-08 03:54:48 +0200

    Page de capture de devchimp

--- php/process_form.php
+++ php/process_form.php
@@ -1,20 +1,31 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = "root";
-    $password = "";
-    $con = new PDO('mysql:host=localhost;dbname=chat', $username, $password);
+    $password = "root";
+    $bdd = new PDO('mysql:host=localhost;dbname=devchimp', $username, $password);
     $nom = htmlspecialchars($_POST['nom']);
     $prenom = htmlspecialchars($_POST['prenom']);
     $email = htmlspecialchars($_POST['email']);
-    $sendMessage = $con->prepare("INSERT INTO `message` (`nom`,`prenom`,`email`) VALUES ('$nom','$prenom','$email')");
-    $sendMessage->execute();
-    $con = null;
+    $isContactInDb = $bdd->prepare("SELECT `email` FROM `contact_capture` WHERE `email` = '$email'");
+    $isContactInDb->execute();
+    $bdd = null;
+    $rowEmail = $isContactInDb->rowCount();
 
-    // Rediriger l'utilisateur vers une page de confirmation
-    header('Location: merci.html');
+    if ($rowEmail == 0) {
+        $bdd = new PDO('mysql:host=localhost;dbname=devchimp', $username, $password);
+        $sendMessage = $bdd->prepare("INSERT INTO `contact_capture` (`nom`,`prenom`,`email`) VALUES ('$nom','$prenom','$email')");
+        $sendMessage->execute();
+        $bdd = null;
+
+        // Rediriger l'utilisateur vers une page de confirmation
+        header('Location: ../merci.html');
+        exit();
+    }
+
+    header('Location: ../index.html');
     exit();
 } else {
-    header('Location: index.html');
+    header('Location: ../index.html');
     exit();
 }
 ?>
